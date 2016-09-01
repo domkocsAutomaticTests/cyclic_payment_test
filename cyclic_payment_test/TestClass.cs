1d9ec9bbb1d9b92f943cfd8c93f39647bcd276be
ap-dominik.kocka <ap-dominik.kocka@egroup.hu>
2016-09-01 12:21:30 +0200

    added directdebit module

--- cyclic_payment_test/TestClass.cs
+++ cyclic_payment_test/TestClass.cs
@@ -16,9 +16,12 @@ namespace SeleniumTests
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
-        private int dom = 0, sep = 0, swi = 0, zus = 0, tax = 0;
-        private DateTime datum = DateTime.Today;
-        private DateTime holnap = DateTime.Today.AddDays(2);
+        private int dom = 0, sep = 0, swi = 0, zus = 0, tax = 0, dd = 0;
+        private DateTime ma = DateTime.Today;
+        /*private DateTime = GetTomorrow();
+        private DateTime datum = DateTime.Today.AddYears(10);
+        */
+        private string[] datum = { "2017", "08", "30" }; // = 20200901;
 
         [SetUp]
         public void SetupTest()
@@ -36,9 +39,7 @@ namespace SeleniumTests
                 driver.Quit();
             }
             catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
+            { }
             Assert.AreEqual("", verificationErrors.ToString());
         }
         [Test]
@@ -67,7 +68,7 @@ namespace SeleniumTests
             /*Cyclic payment +*/
             driver.FindElement(By.Id("Input_StandingOrder")).Click();
             driver.FindElement(By.Id("calendarStartingDate")).Clear();
-            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(holnap.ToString());
+            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(datum[0].ToString() + "-" + datum[1].ToString() + "-" + datum[2].ToString());
             driver.FindElement(By.Id("Input_CyclePeriod")).Clear();
             driver.FindElement(By.Id("Input_CyclePeriod")).SendKeys("1");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -125,7 +126,7 @@ namespace SeleniumTests
             /*Cyclic payment +*/
             driver.FindElement(By.Id("Input_StandingOrder")).Click();
             driver.FindElement(By.Id("calendarStartingDate")).Clear();
-            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(holnap.ToString());
+            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(datum[0].ToString() + "-" + datum[1].ToString() + "-" + datum[2].ToString());
             driver.FindElement(By.Id("Input_CyclePeriod")).Clear();
             driver.FindElement(By.Id("Input_CyclePeriod")).SendKeys("1");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -175,7 +176,7 @@ namespace SeleniumTests
             /*Cyclic payment +*/
             driver.FindElement(By.Id("Input_StandingOrder")).Click();
             driver.FindElement(By.Id("calendarStartingDate")).Clear();
-            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(holnap.ToString());
+            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(datum[0].ToString() + "-" + datum[1].ToString() + "-" + datum[2].ToString());
             driver.FindElement(By.Id("Input_CyclePeriod")).Clear();
             driver.FindElement(By.Id("Input_CyclePeriod")).SendKeys("1");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -218,7 +219,7 @@ namespace SeleniumTests
             driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
             driver.FindElement(By.Id("Input_DecisionAgreement")).Clear();
             driver.FindElement(By.Id("Input_DecisionAgreement")).SendKeys("40");
-            datum.ToString().ToCharArray();
+            ma.ToString().ToCharArray();
             char[] tomb = new char[6];
             int szam = 0;
             for (int i = 0; i < 8; i++)
@@ -228,7 +229,7 @@ namespace SeleniumTests
                     i = 6;
                 }
                 string id = "zusDate" + ((szam + 1).ToString());
-                driver.FindElement(By.Id(id)).SendKeys((datum.ToString().ToCharArray()[i]).ToString());
+                driver.FindElement(By.Id(id)).SendKeys((ma.ToString().ToCharArray()[i]).ToString());
                 szam++;
             }
             driver.FindElement(By.Id("Input_DeclarationNumber")).Clear();
@@ -239,7 +240,7 @@ namespace SeleniumTests
             driver.FindElement(By.Id("Input_GenerateBefore")).Clear();
             driver.FindElement(By.Id("Input_GenerateBefore")).SendKeys("1");
             driver.FindElement(By.Id("calendarStartingDate")).Clear();
-            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(holnap.ToString());
+            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(datum[0].ToString() + "-" + datum[1].ToString() + "-" + datum[2].ToString());
             driver.FindElement(By.Id("Input_CyclePeriod")).Clear();
             driver.FindElement(By.Id("Input_CyclePeriod")).SendKeys("1");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -292,7 +293,7 @@ namespace SeleniumTests
             driver.FindElement(By.Id("Input_GenerateBefore")).Clear();
             driver.FindElement(By.Id("Input_GenerateBefore")).SendKeys("1");
             driver.FindElement(By.Id("calendarStartingDate")).Clear();
-            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(holnap.ToString());
+            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(datum[0].ToString() + "-" + datum[1].ToString() + "-" + datum[2].ToString());
             driver.FindElement(By.Id("Input_CyclePeriod")).Clear();
             driver.FindElement(By.Id("Input_CyclePeriod")).SendKeys("1");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -316,6 +317,59 @@ namespace SeleniumTests
                 verificationErrors.Append(e.Message);
             }
         }
+        [Test]
+        public void DirectDebitCyclicTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/Login/Login");
+            driver.FindElement(By.LinkText("Login with RSA token [DEMO]")).Click();
+            driver.FindElement(By.Id("loginId")).Clear();
+            driver.FindElement(By.Id("loginId")).SendKeys("100003");
+            driver.FindElement(By.Id("login")).Click();
+            try
+            {
+                driver.FindElement(By.Id("submit")).Click();
+            }
+            catch { }
+            dd = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[7]/td[2]")).Text);
+            driver.Navigate().GoToUrl(baseURL + "/DirectDebit/New");
+            driver.FindElement(By.Id("Input_BnName-Search")).Click(); Thread.Sleep(3000);
+            driver.FindElement(By.Id("gvDirectDebitPartnerSearch_DXDataRow0")).Click(); Thread.Sleep(1000);
+            driver.FindElement(By.Id("Input_Details")).Clear();
+            driver.FindElement(By.Id("Input_Details")).SendKeys("automaticTest");
+            driver.FindElement(By.Id("Input_ExtRef")).Clear();
+            driver.FindElement(By.Id("Input_ExtRef")).SendKeys("automaticTest");
+            driver.FindElement(By.Id("Input_Amount_formatted")).Clear();
+            driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
+            driver.FindElement(By.Id("Input_StandingOrder")).Click();
+            driver.FindElement(By.Id("Input_GenerateBefore")).Clear();
+            driver.FindElement(By.Id("Input_GenerateBefore")).SendKeys("1");
+            driver.FindElement(By.Id("calendarStartingDate")).Clear();
+            driver.FindElement(By.Id("calendarStartingDate")).SendKeys(datum[0].ToString() + "-" + datum[1].ToString() + "-" + datum[2].ToString());
+            driver.FindElement(By.Id("Input_CyclePeriod")).Clear();
+            driver.FindElement(By.Id("Input_CyclePeriod")).SendKeys("1");
+            driver.FindElement(By.Id("actionButton_Save")).Click();
+            try
+            {
+                driver.FindElement(By.Id("actionButton_Save")).Click();
+                Thread.Sleep(1000);
+                driver.Navigate().GoToUrl(baseURL + "/Home/Dashboard");
+            }
+            catch (Exception)
+            {
+                driver.Navigate().GoToUrl(baseURL + "/Home/Dashboard");
+            }
+            Thread.Sleep(1000);
+            try
+            {
+                Assert.AreEqual((dd + 1).ToString(), driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[7]/td[2]")).Text);
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+
+        }
+
         private bool IsElementPresent(By by)
         {
             try
